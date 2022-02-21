95f0bfade7dac25d3c43e9e0989d2ddd31893fac
yashas3013 <playyash1@gmail.com>
2022-02-21 19:21:17 +0530

    cleaning

--- mov/src/hsl.py
+++
@@ -1,6 +0,0 @@
-import cv2 as cv
-import numpy as np
-
-file = "/home/ros/catkin_ws/src/mov/src/unknown.png"
-img = cv.imread(file)
-hsl = cv.cvtColor(img,cv.COLOR_)
--- mov/src/thresh.py
+++
@@ -1,4 +0,0 @@
-from skimage import io
-def show(img):
-    io.imshow(img)
-    io.show()
